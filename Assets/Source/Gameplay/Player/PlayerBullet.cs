61ec6744e5224361fb0cea18b6053822a9deb7b3
Blur <pop297197228@163.com>
2025-03-02 21:31:42 +0900

    【Player】 DeadZone for bullet.

--- Assets/Source/Gameplay/Player/PlayerBullet.cs
+++ Assets/Source/Gameplay/Player/PlayerBullet.cs
@@ -25,4 +25,15 @@ public class PlayerBullet : FlyItemBase
             GameMode.Instance.AddScore(damageable.GetScore());
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Destroy the bullet when it leaves the dead zone to prevent it from hitting enemies outside the screen.
+        // 当子弹离开死区时销毁自身，防止攻击到屏幕外的敌人。
+        //　弾丸がデッドゾーンを出た際に自壊し、画面外の敵を攻撃しないようにする。
+        if (other.CompareTag("DeadZone"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
