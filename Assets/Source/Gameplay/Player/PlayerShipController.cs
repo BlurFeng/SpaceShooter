9190a5b80bb8446ff26d9eb4ddbbc1f6f2dbadcc
Blur <pop297197228@163.com>
2025-03-02 16:48:13 +0900

    【Player】 Generate bullets in different ways based on the current shooter level.

--- Assets/Source/Gameplay/Player/PlayerShipController.cs
+++ Assets/Source/Gameplay/Player/PlayerShipController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerShipController : MonoBehaviour, IDamageable
 {
@@ -37,7 +36,64 @@ public class PlayerShipController : MonoBehaviour, IDamageable
     [SerializeField]
     private AudioSource shootAudio;
 
+    [SerializeField]
+    private int shooterLevel = 1;
+    
     private float shootTimePoint;
+
+    /// <summary>
+    /// After shooter level up, we can fire more bullets at once.
+    /// 升级发射器后我们能一次性发射更多的子弹。
+    /// Shooterをレベルアップすると、一度にもっと多くの弾を発射できるようになります。
+    /// </summary>
+    public void ShooterLevelUp()
+    {
+        shooterLevel++;
+    }
+
+    private void Shoot()
+    {
+        // Check the firing interval time.
+        // 确认发射间隔时间。
+        // 発射間隔時間を確認する。
+        if(Time.time < shootTimePoint) return;
+        shootTimePoint = Time.time + shootInterval;
+
+        // Generate bullets in different ways based on the current shooter Level.
+        // 根据当前发射器等级，以不同的方式生成子弹。
+        // 現在のshooterのレベルに基づいて、異なる方法で弾を生成する。
+        if (shooterLevel == 1)
+        {
+            SpawnBullet();
+        }
+        else if (shooterLevel == 2)
+        {
+            SpawnBullet(30f);
+            SpawnBullet(-30f);
+
+        }
+        else if (shooterLevel >= 3)
+        {
+            SpawnBullet();
+            SpawnBullet(30f);
+            SpawnBullet(-30f);
+        }
+        
+        shootAudio.Play();
+    }
+
+    private void SpawnBullet(float angleOffset = 0f)
+    {
+        PlayerBullet playerBullet = Instantiate(bulletPrefab, shootTransform.position, shootTransform.rotation).GetComponent<PlayerBullet>();
+        playerBullet.Init(tag);
+
+        if (angleOffset != 0f)
+        {
+            playerBullet.gameObject.transform.Rotate(Vector3.forward, angleOffset);
+            playerBullet.SetCustomFlyDirection(BlurFunctionLibrary.AngleAxis(Vector3.right, Vector3.forward, angleOffset));
+        }
+    }
+    
     #endregion
     
     // Start is called before the first frame update
@@ -56,12 +112,9 @@ public class PlayerShipController : MonoBehaviour, IDamageable
         inputDir = new Vector3(h, v, 0f).normalized;
 
         // --- Shoot ---
-        if (Input.GetButton("Fire1") && Time.time > shootTimePoint)
+        if (Input.GetButton("Fire1"))
         {
-            shootTimePoint = Time.time + shootInterval;
-            var bullet = Instantiate(bulletPrefab, shootTransform.position, shootTransform.rotation);
-            bullet.GetComponent<PlayerBullet>().Init(tag);
-            shootAudio.Play();
+            Shoot();
         }
     }
 
